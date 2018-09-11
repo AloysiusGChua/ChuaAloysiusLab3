cae42cfa8f2cf6d202f3bd2214cb757168a09f18
AloysiusGChua <2015081433@ust-ics.mygbiz.com>
2018-09-11 19:03:15 +0800

    Initial commit

---
+++ app/src/main/java/com/Chua/Aloysius/StylesandMaterialsMain.java
@@ -0,0 +1,13 @@
+package com.Chua.Aloysius;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StylesandMaterialsMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
